7277418feff03bc9bd834254db0dce920378e2f8
Mauricio Tomaz <mtomaz@whiteoutsolutions.com>
2022-06-14 14:56:35 -0700

    Implement OOP exercises week4/day2

--- week4/day2/pizza.js
+++ week4/day2/pizza.js
@@ -1,5 +1,33 @@
 class Pizza {
-  constructor() {
-    this.toppings = ['cheese'];
+  /**
+   * @param {number} size
+   * @param {...string} toppings
+   */
+  constructor(size = 8, ...toppings) {
+    this.size = size;
+    this.toppings = ["cheese", ...toppings];
+  }
+
+  /**
+   * @param  {...string} toppings
+   */
+  addToppings(...toppings) {
+    if (!toppings) {
+      return;
+    }
+
+    this.toppings = [...this.toppings, ...toppings];
+  }
+
+  /**
+   * @returns {string} number of people this pizza size is ideal for
+   */
+  goodFor() {
+    return `This size is perfect for ${this.size / 2} people`;
   }
 }
+
+const marguerita = new Pizza();
+marguerita.addToppings("basil", "tomato");
+console.log(marguerita);
+console.log(marguerita.goodFor());
